28cf6050d3c6c47246bed87e8a710dec002675b6
oldens <o.denyschuk@24tv.com.ua>
2025-02-21 10:47:55 +0200

    add listeners for button

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import Game from './game.js';
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    const myGame = new Game('FirstGame');
-    const myGame2 = new Game('SecondGame');
-    const myGame3 = new Game('ThirdGame');
+    const myGame = new Game('First Game');
+    const myGame2 = new Game('Second Game');
+    const myGame3 = new Game('Third Game');
     
 });
--- game.js
+++ game.js
@@ -4,23 +4,51 @@ class Game {
     this.playersWins = 0;
     this.dealerWins = 0;
     console.log("Game constructor " + this.name);
-    this.ontainer = this.createContainer();
+    this.id = "game_" + this.name.replace(" ", "_");
+    this.container = this.createContainer();
+
+    this.addListeners();
   }
 
   createContainer() {
     const container = document.createElement("div");
-    container.id = "game" + this.name;
+    container.id = this.id;
     container.className = "game";
     container.innerHTML = `<h1>${this.name}</h1>
-    <button id="deal">Deal</button>
-    <button id="hit">Hit</button>
-    <button id="stand">Stand</button>
-    <div id="players">players area</div>
-    <div id="result">result area</div>
+    <button id="deal_${this.id}">Deal</button>
+    <button id="hit_${this.id}">Hit</button>
+    <button id="stand_${this.id}">Stand</button>
+    <div id="players_${this.id}">players area</div>
+    <div id="result_${this.id}">result area</div>
     `;
     document.body.appendChild(container);
     return container;
   }
+
+
+  addListeners() {
+    const dealButton = document.getElementById("deal_"+this.id);
+    dealButton.addEventListener("click", this.deal.bind(this));
+    const hitButton = document.getElementById("hit_" + this.id);
+    hitButton.addEventListener("click", this.hit.bind(this));
+    const standButton = document.getElementById("stand_" + this.id);
+    standButton.addEventListener("click", this.stand.bind(this));
+  }
+
+  deal() {
+    console.log("Deal");
+  }
+
+  stand() {
+    console.log("Stand");
+  }
+
+  hit() {
+    console.log("Hit");
+  } 
+
+
+
 }
 
 export default Game;
